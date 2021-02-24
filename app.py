a95010fb0556ecaf292c7534a9f1a1d5e7c0d131
Paola Romero <PaolaRomero@paolas-mbp.lan>
2021-02-23 20:28:25 -0600

    Finished all routes

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ session = Session(engine)
 
 # Set up Flask
 app = Flask(__name__)
+
 # Create welcome route
 @app.route("/")
 # Create function for adding routing info to each of the routes
@@ -41,3 +42,50 @@ def precipitation():
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
+
+# Create stations route
+@app.route("/api/v1.0/stations")
+# Create the stations function
+def stations():
+    # Create a query that will get all the stations in the database
+    results = session.query(Station.station).all()
+    # Unravel the results into a one-dimensional array with function np.ravel() with results as parameter
+    # then, convert the unraveled results into a list
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# Create temperature route
+@app.route("/api/v1.0/tobs")
+# Create the temperature function
+def temp_monthly():
+    # calculate the date one year ago from the last date in the database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # query the primary station for all the temp observations from the previous year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    # Unravel the results into a one-dimensional array and convert array to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# Create summary statistics route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+# Create stats function, add a start parameter and end parameter
+def stats(start=None, end=None):
+    # create a query to select the minimum, average, and maximum temperatures from our SQLite database
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    # to determine start and end date, use if-not statement
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
